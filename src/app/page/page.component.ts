19d5e3793d29220d743439c7a9cd35918133e41a
Chris Wong <wc38.wong.public@gmail.com>
2019-01-24 12:07:14 +0800

    Search with name

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -8,17 +8,26 @@ import { PokemonService } from '../pokemon.service';
   styleUrls: ['./page.component.css']
 })
 export class PageComponent implements OnInit {
+  _pokemons : Pokemon[];
   pokemons : Pokemon[];
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
     this.pokemonService.getPokemons().subscribe((data: any) =>
     {
-      this.pokemons = data.results.map((pokemon: any) => {
+      this._pokemons = data.results.map((pokemon: any) => {
         let { url } = pokemon;
         pokemon.id = url.substring(34, url.length - 1);
         return pokemon;
       })
+
+      this.pokemons = this._pokemons;
+    });
+  }
+
+  filterPokemons(searchString: string) {
+    this.pokemons = this._pokemons.filter(pokemon => {
+      return pokemon.name.includes(searchString.toLowerCase());
     });
   }
 }
